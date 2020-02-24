c0e3a6fbe74c55c579484bafa050f1a9e4d0bffc
davisjustinw <davisjustinw@gmail.com>
2020-02-24 07:23:32 -0800

    add recipe user seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_192957) do
+ActiveRecord::Schema.define(version: 2020_02_24_043919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipe_users", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+    t.string "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
