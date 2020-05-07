7bc00866c04846a18a2282240760500cb7a7eb45
davisjustinw <davisjustinw@gmail.com>
2020-05-06 22:25:31 -0700

    add avatar and build date to history list

--- client/src/layout/HistoryContents.js
+++ client/src/layout/HistoryContents.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { changeCurrentMake } from '../stores/recipe/makeActions'
 import { makeStyles } from '@material-ui/core/styles'
+import { url } from '../stores/helpers/fetchHelpers'
+
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
 import ListItemText from '@material-ui/core/ListItemText'
 
 import Hidden from '@material-ui/core/Hidden'
@@ -29,11 +33,21 @@ const HistoryContents =
           makes.map(make => {
             return (
               <ListItem
+                alignItems="flex-start"
                 key={make.id}
                 selected={make.id === currentMake.id}
                 button onClick={e => handleHistoryClick(make.id, e)}
               >
-                <ListItemText primary={make.alias} />
+                <ListItemAvatar>
+                  <Avatar
+                    alt='cook'
+                    src={`${url}${make.cook.avatarUrl}`}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={make.alias}
+                  secondary={make.updatedAt}
+                />
               </ListItem>
             )
           })
